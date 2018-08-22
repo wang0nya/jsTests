9772f651d54ade73edc64b175a796bef7e1da8bb
wang0nya <kwangonya@gmail.com>
2018-08-22 09:04:58 +0300

    use arrow function

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
 //simple sort algorithm
 const values = [8,4,2,3,9,5,1,6,7,0];
-values.sort(function(value1, value2) {
+values.sort((value1, value2) => {
         return value1 - value2;
     });
 console.log(values);
